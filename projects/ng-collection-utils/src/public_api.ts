d09a67fc50d531bef2fe3598b181e05da23f4a09
guillem <guillem.cordoba@gmail.com>
2018-07-15 20:53:21 +0200

    Bootstraped library

---
+++ projects/ng-collection-utils/src/public_api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of ng-filter-utils
+ */
+
+export * from './lib/chips-autocomplete/chips-autocomplete.component';
+export * from './lib/animations';
+export * from './lib/loading';
+export * from './lib/sorting';
+export * from './lib/filtering';
+export * from './lib/ng-collection-utils.module';
